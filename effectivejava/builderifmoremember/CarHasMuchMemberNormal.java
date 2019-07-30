e4bd295d3dcecea881d8dc6946793456f9d7e8c7
wyett <wyettlei@hotmail.com>
2019-07-30 11:47:06 +0800

    modify this(a,b,c,0)

--- effectivejava/builderifmoremember/CarHasMuchMemberNormal.java
+++ effectivejava/builderifmoremember/CarHasMuchMemberNormal.java
@@ -19,18 +19,20 @@ public class CarHasMuchMemberNormal {
     public CarHasMuchMemberNormal(String brand,
                                   String color,
                                   String lisencePlateNumber) {
-        this(brand, color, lisencePlateNumber);
+        this(brand, color, lisencePlateNumber, 0);
     }
-    public CarHasMuchMemberNormal(String brand, String color, String lisencePlateNumber,
+    public CarHasMuchMemberNormal(String brand,
+                                  String color,
+                                  String lisencePlateNumber,
                                   int length) {
-        this(brand, color, lisencePlateNumber, length);
+        this(brand, color, lisencePlateNumber, length, 0);
     }
     public CarHasMuchMemberNormal(String brand,
                                   String color,
                                   String lisencePlateNumber,
                                   int length,
                                   int width) {
-        this(brand, color, lisencePlateNumber, length, width);
+        this(brand, color, lisencePlateNumber, length, width, 0);
     }
     public CarHasMuchMemberNormal(String brand,
                                   String color,
