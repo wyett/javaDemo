2b51d68d3aada155b6c1036eee5a9916c2a4f0d7
wyett <wyettlei@hotmail.com>
2019-08-09 18:15:22 +0800

    fix:

--- datastructuresandalgorithms/list/example/MySingleLinkedList.java
+++ datastructuresandalgorithms/list/example/MySingleLinkedList.java
@@ -31,35 +31,64 @@ public class MySingleLinkedList<E> {
     // size
     private int theSize;
 
+    /** init */
     public MySingleLinkedList() {
         doClear();
     }
 
+    /**
+     * do clear
+     */
     public void doClear() {
         clear();
     }
+
+    /**
+     * clear
+     */
     public void clear() {
         header.next = tailer;
         tailer.next = null;
         theSize = 0;
     }
 
+    /**
+     * check if list is empty
+     * @return
+     */
     public boolean isEmpty() {
         return theSize == 0;
     }
 
+    /**
+     * @return size of list
+     */
     public int size() {
         return theSize;
     }
 
+    /**
+     * add x into list at the tail
+     * @param x
+     */
     public void add(E x) {
         addAfter(getNode(size() - 1), x);
     }
 
+    /**
+     * add x into list at idx pos
+     * @param idx
+     * @param x node.data
+     */
     public void add(int idx, E x) {
         addAfter(getNode(idx), x);
     }
 
+    /**
+     * add node with x after node n
+     * @param n node
+     * @param x newnode.data
+     */
     public void addAfter(Node<E> n, E x) {
         Node<E> newNode = new Node<E>(x, null);
         newNode.next = n.next;
@@ -67,10 +96,21 @@ public class MySingleLinkedList<E> {
         theSize++;
     }
 
+    /**
+     * add collection into list at the tail
+     * @param c
+     * @return
+     */
     public boolean addAll(java.util.Collection<? extends E> c) {
         return addAll(theSize, c);
     }
 
+    /**
+     * insert collection into list at pos idx
+     * @param idx
+     * @param c
+     * @return
+     */
     public boolean addAll(int idx, java.util.Collection<? extends E> c) {
         if(idx < 0 || idx > size()) {
             throw new IndexOutOfBoundsException();
@@ -88,6 +128,11 @@ public class MySingleLinkedList<E> {
         return true;
     }
 
+    /**
+     * get the idx element
+     * @param idx
+     * @return Node
+     */
     public Node<E> getNode(int idx) {
         checkPositionIndex(idx);
 
@@ -100,6 +145,11 @@ public class MySingleLinkedList<E> {
         return tmpNode;
     }
 
+    /**
+     * remove the idx element
+     * @param idx
+     * @return node.data
+     */
     public E remove(int idx) {
         checkPositionIndex(idx);
 
@@ -122,6 +172,18 @@ public class MySingleLinkedList<E> {
         return tmpData;
     }
 
+    /**
+     * remove the tail element
+     * @return
+     */
+    public E removeLast() {
+        return remove(theSize);
+    }
+
+    /**
+     * check if pos idx is proper
+     * @param idx
+     */
     public void checkPositionIndex(int idx) {
         if(idx < 0 || idx > theSize) {
             throw new IndexOutOfBoundsException();
