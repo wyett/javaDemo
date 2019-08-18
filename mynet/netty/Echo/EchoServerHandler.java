8b2eec11e660e007bc90f89465549f18e5149fd5
wyett <wyettlei@hotmail.com>
2019-08-18 18:48:23 +0800

    example Echo

---
+++ mynet/netty/Echo/EchoServerHandler.java
@@ -0,0 +1,43 @@
+package mynet.netty.Echo;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+/**
+ * @author : wyettLei
+ * @date : Created in 2019/8/18 16:16
+ * @description:
+ */
+
+@ChannelHandler.Sharable
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+    // all message will be called
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ByteBuf in = (ByteBuf)msg;
+        // console
+        System.out.println(
+                "Server received: " + in.toString(CharsetUtil.UTF_8));
+        // tell sender
+        ctx.write(in);
+    }
+
+    // tail ChannelInboundHandler this message is last message
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER)
+                .addListener(ChannelFutureListener.CLOSE);
+    }
+
+    // throw exception while reading msg
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+}
