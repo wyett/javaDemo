a7efd9d451d6af6961a8dab447a05826c55d9594
wyett <wyettlei@hotmail.com>
2019-09-02 11:01:30 +0800

    nio demo

---
+++ mynet/netty/NioDemo/NettyNioServer.java
@@ -0,0 +1,57 @@
+package mynet.netty.NioDemo;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.charset.Charset;
+
+/**
+ * @author : wyettLei
+ * @date : Created in 2019/8/30 15:11
+ * @description: TODO
+ */
+
+public class NettyNioServer {
+    public void server(int port) throws Exception {
+        final ByteBuf buf = Unpooled.unreleasableBuffer(
+                Unpooled.copiedBuffer("Hi!\r\n", Charset.forName("UTF-8")));
+        EventLoopGroup elg = new NioEventLoopGroup();
+        try {
+            ServerBootstrap sb = new ServerBootstrap();
+            sb.group(elg)
+                    .channel(NioServerSocketChannel.class)
+                    .localAddress(new InetSocketAddress(port))
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch) throws
+                                Exception {
+                            ch.pipeline().addLast(
+                                    new ChannelInboundHandlerAdapter() {
+                                        @Override
+                                        public void channelActive(
+                                                ChannelHandlerContext ctx)
+                                                throws Exception {
+                                            ctx.writeAndFlush(buf.duplicate())
+                                                    .addListener(
+                                                            ChannelFutureListener.CLOSE);
+                                        }
+                                    });
+                        }
+                    });
+            ChannelFuture f = sb.bind().sync();
+            f.channel().closeFuture().sync();
+        } finally {
+            elg.shutdownGracefully().sync();
+        }
+    }
+}
---
+++ mynet/netty/NioDemo/PlainNioServer.java
@@ -0,0 +1,96 @@
+package mynet.netty.NioDemo;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.nio.ByteBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Iterator;
+import java.util.Set;
+
+/**
+ * @author : wyettLei
+ * @date : Created in 2019/8/30 11:43
+ * @description: TODO
+ */
+
+public class PlainNioServer {
+    public void serve(int port) throws IOException {
+        // open channel
+        ServerSocketChannel ssc = ServerSocketChannel.open();
+        ssc.configureBlocking(false);
+
+        // create socket
+        ServerSocket ssocket = ssc.socket();
+        InetSocketAddress address = new InetSocketAddress(port);
+        ssocket.bind(address);
+
+        // create selector
+        Selector selector = Selector.open();
+        // register ssc on selector
+        ssc.register(selector, SelectionKey.OP_ACCEPT);
+
+        //create new bytebuffer
+        final ByteBuffer msg =
+                ByteBuffer.wrap("Hi!\r\n".getBytes("UTF-8"));
+
+        for(;;) {
+            try {
+                // blocking, and waiting for next case
+                selector.select();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+                break;
+            }
+
+            // receive all selectedKeys
+            Set<SelectionKey> readyKeys = selector.selectedKeys();
+            Iterator<SelectionKey> iterator = readyKeys.iterator();
+            while(iterator.hasNext()) {
+                SelectionKey key = iterator.next();
+                iterator.remove();
+
+                try {
+                    // check if is acceptable
+                    if(key.isAcceptable()) {
+                        ServerSocketChannel server =
+                                (ServerSocketChannel) key.channel();
+                        SocketChannel client = server.accept();
+                        client.configureBlocking(false);
+                        // accept and register
+                        client.register(selector, SelectionKey.OP_WRITE |
+                            SelectionKey.OP_READ, msg.duplicate());
+                        System.out.println(
+                                "Accepted connection from " + client);
+                    }
+                    // check if writable
+                    if (key.isWritable()) {
+                        SocketChannel client =
+                                (SocketChannel) key.channel();
+                        ByteBuffer buffer =
+                                (ByteBuffer) key.attachment();
+                        while (buffer.hasRemaining()) {
+                            if (client.write(buffer) == 0) {
+                                break;
+                            }
+                        }
+                        client.close();
+                    }
+                } catch(IOException ex) {
+                    key.cancel();
+                    try {
+                        key.channel().close();
+                    } catch (IOException cex) {
+
+                    }
+                }
+            }
+        }
+
+
+
+    }
+}
