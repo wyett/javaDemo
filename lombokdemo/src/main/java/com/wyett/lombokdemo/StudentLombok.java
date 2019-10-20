7e6aae6993f05cc98edffc12b87c7806c499c7e6
wyettlei@hotmail.com <wyettlei@hotmail.com>
2019-10-20 14:37:46 +0800

    lombok demo

---
+++ lombokdemo/src/main/java/com/wyett/lombokdemo/StudentLombok.java
@@ -0,0 +1,17 @@
+package com.wyett.lombokdemo;
+
+import lombok.*;
+
+/**
+ * @author : wyettLei
+ * @date : Created in 2019/9/9 15:55
+ * @description: TODO
+ */
+
+@Setter
+@Getter
+@ToString
+public class StudentLombok {
+    private String name;
+    private int age;
+}
---
+++ lombokdemo/src/main/java/com/wyett/lombokdemo/StudentTest.java
@@ -0,0 +1,18 @@
+package com.wyett.lombokdemo;
+
+
+import org.junit.Test;
+
+/**
+ * @author : wyettLei
+ * @date : Created in 2019/9/9 16:33
+ * @description: TODO
+ */
+
+public class StudentTest {
+    @Test
+    public static void Test() {
+        StudentLombok sl = new StudentLombok();
+        System.out.println(sl);
+    }
+}
